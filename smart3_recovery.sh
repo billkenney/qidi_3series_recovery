084c63a66bdac1dde769cc4c902f855394328c98
billkenney <30010560+billkenney@users.noreply.github.com>
2024-05-27 15:15:47 -0500

    Update smart3_recovery.sh

--- smart3_recovery.sh
+++ smart3_recovery.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 cd /home/mks
 sudo sed -i 's/deb.debian.org\/debian buster-backports/archive.debian.org\/debian buster-backports/g' /etc/apt/sources.list
-wget --no-check-certificate https://raw.githubusercontent.com/billkenney/max3_plus3_recovery/main/800_480-smart3.tft
-wget --no-check-certificate https://raw.githubusercontent.com/billkenney/max3_plus3_recovery/main/mksclient-smart3.deb
+wget --no-check-certificate https://raw.githubusercontent.com/billkenney/qidi_3series_recovery/main/800_480-smart3.tft
+wget --no-check-certificate https://raw.githubusercontent.com/billkenney/qidi_3series_recovery/main/mksclient-smart3.deb
 sudo mv ~/klipper_config/MKS_THR.cfg ~/klipper_config/MKS_THR.cfg.bak ; path=$(ls /dev/serial/by-id/*) ; printf "[mcu MKS_THR]\nserial:$path\n" > ~/klipper_config/MKS_THR.cfg
 sudo dpkg -i mksclient-smart3.deb ; rm mksclient-smart3.deb ; sudo mv 800_480-smart3.tft /root/800_480.tft
-
 read -p 'do you want to update your printer.cfg file? (y/n): ' pcfg
 if [[ "$pcfg" =~ [yY] ]] ; then
-  wget --no-check-certificate https://raw.githubusercontent.com/billkenney/max3_plus3_recovery/main/printer-smart3.cfg
+  wget --no-check-certificate https://raw.githubusercontent.com/billkenney/qidi_3series_recovery/main/printer-smart3.cfg
   mv ~/klipper_config/printer.cfg ~/klipper_config/printer.cfg.bak ; mv printer-smart3.cfg ~/klipper_config/printer.cfg
   echo "your old printer.cfg has been saved to ~/klipper_config/printer.cfg.bak"
 fi
