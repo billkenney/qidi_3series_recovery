57eb5e49fc1b5d6b7fd591d190659b7f5da7ac58
billkenney <30010560+billkenney@users.noreply.github.com>
2024-05-27 15:05:32 -0500

    Update max3_probe_recovery.sh

--- max3_probe_recovery.sh
+++ max3_probe_recovery.sh
@@ -1,20 +1,19 @@
 #!/bin/bash
 read -p 'do you want to update your printer.cfg file? (y/n): ' pcfg
 if [[ "$pcfg" =~ [yY] ]] ; then
-  wget --no-check-certificate https://raw.githubusercontent.com/billkenney/max3_plus3_recovery/main/printer-max3_probe.cfg
-  mv ~/klipper_config/printer.cfg ~/klipper_config/printer.cfg.bak
-  mv printer-max3_probe.cfg ~/klipper_config/printer.cfg
   sudo sed -i 's/deb.debian.org\/debian buster-backports/archive.debian.org\/debian buster-backports/g' /etc/apt/sources.list
+  wget --no-check-certificate https://raw.githubusercontent.com/billkenney/max3_plus3_recovery/main/printer-max3_probe.cfg
+  mv ~/klipper_config/printer.cfg ~/klipper_config/printer.cfg.bak ; mv printer-max3_probe.cfg ~/klipper_config/printer.cfg
   wget --no-check-certificate https://raw.githubusercontent.com/billkenney/max3_plus3_recovery/main/800_480.tft
   wget --no-check-certificate https://raw.githubusercontent.com/billkenney/max3_plus3_recovery/main/mksclient-max3.deb
-  sudo dpkg -i mksclient-max3.deb
-  sudo mv 800_480.tft /root/
+  sudo dpkg -i mksclient-max3.deb ; rm mksclient-max3.deb ; sudo mv 800_480.tft /root/
+  sudo mv ~/klipper_config/MKS_THR.cfg ~/klipper_config/MKS_THR.cfg.bak ; path=$(ls /dev/serial/by-id/*) ; printf "[mcu MKS_THR]\nserial:$path\n" > ~/klipper_config/MKS_THR.cfg
 elif [[ "$pcfg" =~ [nN] ]] ; then
   sudo sed -i 's/deb.debian.org\/debian buster-backports/archive.debian.org\/debian buster-backports/g' /etc/apt/sources.list
   wget --no-check-certificate https://raw.githubusercontent.com/billkenney/max3_plus3_recovery/main/800_480.tft
   wget --no-check-certificate https://raw.githubusercontent.com/billkenney/max3_plus3_recovery/main/mksclient-max3.deb
-  sudo dpkg -i mksclient-max3.deb
-  sudo mv 800_480.tft /root/
+  sudo dpkg -i mksclient-max3.deb ; rm mksclient-max3.deb ; sudo mv 800_480.tft /root/
+  sudo mv ~/klipper_config/MKS_THR.cfg ~/klipper_config/MKS_THR.cfg.bak ; path=$(ls /dev/serial/by-id/*) ; printf "[mcu MKS_THR]\nserial:$path\n" > ~/klipper_config/MKS_THR.cfg
 else
   echo "ERROR: invalid response"
   exit 0
