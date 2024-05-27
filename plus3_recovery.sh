e4dc5d970bd575a85355139ad0fb292c0ab37301
billkenney <30010560+billkenney@users.noreply.github.com>
2024-05-27 15:07:19 -0500

    Update plus3_recovery.sh

--- plus3_recovery.sh
+++ plus3_recovery.sh
@@ -2,19 +2,18 @@
 read -p 'do you want to update your printer.cfg file? (y/n): ' pcfg
 if [[ "$pcfg" =~ [yY] ]] ; then
   wget --no-check-certificate https://raw.githubusercontent.com/billkenney/max3_plus3_recovery/main/printer-plus3.cfg
-  mv ~/klipper_config/printer.cfg ~/klipper_config/printer.cfg.bak
-  mv printer-plus3.cfg ~/klipper_config/printer.cfg
+  mv ~/klipper_config/printer.cfg ~/klipper_config/printer.cfg.bak ; mv printer-plus3.cfg ~/klipper_config/printer.cfg
   sudo sed -i 's/deb.debian.org\/debian buster-backports/archive.debian.org\/debian buster-backports/g' /etc/apt/sources.list
   wget --no-check-certificate https://raw.githubusercontent.com/billkenney/max3_plus3_recovery/main/800_480.tft
   wget --no-check-certificate https://raw.githubusercontent.com/billkenney/max3_plus3_recovery/main/mksclient-plus3.deb
-  sudo dpkg -i mksclient-plus3.deb
-  sudo mv 800_480.tft /root/
+  sudo mv ~/klipper_config/MKS_THR.cfg ~/klipper_config/MKS_THR.cfg.bak ; path=$(ls /dev/serial/by-id/*) ; printf "[mcu MKS_THR]\nserial:$path\n" > ~/klipper_config/MKS_THR.cfg
+  sudo dpkg -i mksclient-plus3.deb ; rm mksclient-plus3.deb ; sudo mv 800_480.tft /root/
 elif [[ "$pcfg" =~ [nN] ]] ; then
   sudo sed -i 's/deb.debian.org\/debian buster-backports/archive.debian.org\/debian buster-backports/g' /etc/apt/sources.list
   wget --no-check-certificate https://raw.githubusercontent.com/billkenney/max3_plus3_recovery/main/800_480.tft
   wget --no-check-certificate https://raw.githubusercontent.com/billkenney/max3_plus3_recovery/main/mksclient-plus3.deb
-  sudo dpkg -i mksclient-plus3.deb
-  sudo mv 800_480.tft /root/
+  sudo mv ~/klipper_config/MKS_THR.cfg ~/klipper_config/MKS_THR.cfg.bak ; path=$(ls /dev/serial/by-id/*) ; printf "[mcu MKS_THR]\nserial:$path\n" > ~/klipper_config/MKS_THR.cfg
+  sudo dpkg -i mksclient-plus3.deb ; rm mksclient-plus3.deb ; sudo mv 800_480.tft /root/
 else
   echo "ERROR: invalid response"
   exit 0
