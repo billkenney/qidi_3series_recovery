d6653b75c53f9fd93b96b27c1ffd5b7fff0b82d7
billkenney <30010560+billkenney@users.noreply.github.com>
2024-05-27 15:07:34 -0500

    Update max3_bltouch_recovery.sh

--- max3_bltouch_recovery.sh
+++ max3_bltouch_recovery.sh
@@ -6,14 +6,14 @@ if [[ "$pcfg" =~ [yY] ]] ; then
   mv ~/klipper_config/printer.cfg ~/klipper_config/printer.cfg.bak ; mv printer-max3_bltouch.cfg ~/klipper_config/printer.cfg
   wget --no-check-certificate https://raw.githubusercontent.com/billkenney/max3_plus3_recovery/main/800_480.tft
   wget --no-check-certificate https://raw.githubusercontent.com/billkenney/max3_plus3_recovery/main/mksclient-max3.deb
-  sudo dpkg -i mksclient-max3.deb ; rm mksclient-max3.deb ; sudo mv 800_480.tft /root/
   sudo mv ~/klipper_config/MKS_THR.cfg ~/klipper_config/MKS_THR.cfg.bak ; path=$(ls /dev/serial/by-id/*) ; printf "[mcu MKS_THR]\nserial:$path\n" > ~/klipper_config/MKS_THR.cfg
+  sudo dpkg -i mksclient-max3.deb ; rm mksclient-max3.deb ; sudo mv 800_480.tft /root/
 elif [[ "$pcfg" =~ [nN] ]] ; then
   sudo sed -i 's/deb.debian.org\/debian buster-backports/archive.debian.org\/debian buster-backports/g' /etc/apt/sources.list
   wget --no-check-certificate https://raw.githubusercontent.com/billkenney/max3_plus3_recovery/main/800_480.tft
   wget --no-check-certificate https://raw.githubusercontent.com/billkenney/max3_plus3_recovery/main/mksclient-max3.deb
-  sudo dpkg -i mksclient-max3.deb ; rm mksclient-max3.deb ; sudo mv 800_480.tft /root/
   sudo mv ~/klipper_config/MKS_THR.cfg ~/klipper_config/MKS_THR.cfg.bak ; path=$(ls /dev/serial/by-id/*) ; printf "[mcu MKS_THR]\nserial:$path\n" > ~/klipper_config/MKS_THR.cfg
+  sudo dpkg -i mksclient-max3.deb ; rm mksclient-max3.deb ; sudo mv 800_480.tft /root/
 else
   echo "ERROR: invalid response"
   exit 0
