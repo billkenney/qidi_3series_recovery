11771771b667fedd487b2e7ed3c9ab5dedfaec17
billkenney <30010560+billkenney@users.noreply.github.com>
2024-08-23 16:24:06 -0500

    Update smart3_recovery.sh

--- smart3_recovery.sh
+++ smart3_recovery.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 cd /home/mks
 sudo sed -i 's/deb.debian.org\/debian buster-backports/archive.debian.org\/debian buster-backports/g' /etc/apt/sources.list
-wget --no-check-certificate https://raw.githubusercontent.com/billkenney/qidi_3series_recovery/main/mksclient-smart3.deb
 sudo mv ~/klipper_config/MKS_THR.cfg ~/klipper_config/MKS_THR.cfg.bak ; path=$(ls /dev/serial/by-id/*) ; printf "[mcu MKS_THR]\nserial:$path\n" > ~/klipper_config/MKS_THR.cfg
-sudo dpkg -i mksclient-smart3.deb ; rm mksclient-smart3.deb
 read -p 'do you want to restore the stock klipper_config folder? (y/n): ' kfg
 if [[ "$kfg" =~ [yY] ]] ; then
   tar -czf klipper_config.bak.tgz klipper_config ; rm -rf klipper_config
@@ -20,6 +18,8 @@ elif [[ "$kfg" =~ [nN] ]] ; then
     echo "your old printer.cfg has been saved to ~/klipper_config/printer.cfg.bak"
   fi
 fi
+wget --no-check-certificate https://raw.githubusercontent.com/billkenney/qidi_3series_recovery/main/mksclient-smart3.deb
+sudo dpkg -i mksclient-smart3.deb ; rm mksclient-smart3.deb
 read -p 'do you want to update your screen firmware (unnecessary if its already on the latest firmware, this update takes like 30 min)? (y/n): ' sfmw
 if [[ "$sfmw" =~ [yY] ]] ; then
   wget --no-check-certificate https://raw.githubusercontent.com/billkenney/qidi_3series_recovery/main/800_480-smart3.tft
