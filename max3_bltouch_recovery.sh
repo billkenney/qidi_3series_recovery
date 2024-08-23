7d43d77a9cf96a848f2dc41e3a013a6621056313
billkenney <30010560+billkenney@users.noreply.github.com>
2024-08-23 14:41:31 -0500

    Update max3_bltouch_recovery.sh

--- max3_bltouch_recovery.sh
+++ max3_bltouch_recovery.sh
@@ -4,16 +4,26 @@ sudo sed -i 's/deb.debian.org\/debian buster-backports/archive.debian.org\/debia
 wget --no-check-certificate https://raw.githubusercontent.com/billkenney/qidi_3series_recovery/main/mksclient-max3.deb
 sudo mv ~/klipper_config/MKS_THR.cfg ~/klipper_config/MKS_THR.cfg.bak ; path=$(ls /dev/serial/by-id/*) ; printf "[mcu MKS_THR]\nserial:$path\n" > ~/klipper_config/MKS_THR.cfg
 sudo dpkg -i mksclient-max3.deb ; rm mksclient-max3.deb
+read -p 'do you want to restore the stock klipper_config folder? (y/n): ' kfg
+if [[ "$kfg" =~ [yY] ]] ; then
+  tar -czf klipper_config.bak.tgz klipper_config ; rm -rf klipper_config
+  wget --no-check-certificate https://raw.githubusercontent.com/billkenney/qidi_3series_recovery/main/klipper_config.tgz
+  tar -xzf klipper_config.tgz ; rm klipper_config.tgz
+   wget --no-check-certificate https://raw.githubusercontent.com/billkenney/qidi_3series_recovery/main/printer-max3_bltouch.cfg
+   mv printer-max3_bltouch.cfg ~/klipper_config/printer.cfg
+  echo "your old klipper_config folder has been backed up to klipper_config.bak.tgz and the stock klipper_config folder and printer.cfg have been installed"
+elif [[ "$kfg" =~ [nN] ]] ; then
+  read -p 'do you want to update your printer.cfg file? (y/n): ' pcfg
+  if [[ "$pcfg" =~ [yY] ]] ; then
+    wget --no-check-certificate https://raw.githubusercontent.com/billkenney/qidi_3series_recovery/main/printer-max3_bltouch.cfg
+    mv ~/klipper_config/printer.cfg ~/klipper_config/printer.cfg.bak ; mv printer-max3_bltouch.cfg ~/klipper_config/printer.cfg
+    echo "your old printer.cfg has been saved to ~/klipper_config/printer.cfg.bak"
+  fi
+fi
 read -p 'do you want to update your screen firmware (unnecessary if its already on the latest firmware, this update takes like 30 min)? (y/n): ' sfmw
 if [[ "$sfmw" =~ [yY] ]] ; then
   wget --no-check-certificate https://raw.githubusercontent.com/billkenney/qidi_3series_recovery/main/800_480.tft
   sudo mv 800_480.tft /root/800_480.tft
-fi
-read -p 'do you want to update your printer.cfg file? (y/n): ' pcfg
-if [[ "$pcfg" =~ [yY] ]] ; then
-  wget --no-check-certificate https://raw.githubusercontent.com/billkenney/qidi_3series_recovery/main/printer-max3_bltouch.cfg
-  mv ~/klipper_config/printer.cfg ~/klipper_config/printer.cfg.bak ; mv printer-max3_bltouch.cfg ~/klipper_config/printer.cfg
-  echo "your old printer.cfg has been saved to ~/klipper_config/printer.cfg.bak"
 fi
 sudo chown -R mks:mks /home/mks/klipp* /home/mks/moonraker* /home/mks/fluidd
 
